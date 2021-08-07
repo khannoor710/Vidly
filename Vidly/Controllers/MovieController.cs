c5984de9ae716e278aad0c781be9eb0e791b5b13
khannoor710 <khannoor710@gmail.com>
2021-08-07 13:17:30 +0530

    Added Customers and ViewModel.

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -14,7 +15,19 @@ namespace Vidly.Controllers
         {
             var movie = new Movie { Name = "Shrek" };
 
-            return View(movie);
+            var customers = new List<Customer>()
+            {
+                new Customer{Name="John Smith"},
+                new Customer{Name="Mary Williams"}
+            };
+
+            RandomMovieViewModel randomMovieViewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(randomMovieViewModel);
         }
     }
 }
