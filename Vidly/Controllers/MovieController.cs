8506a2fa368e4c620c55bb63973196b462026dcd
khannoor710 <khannoor710@gmail.com>
2021-08-07 13:10:28 +0530

    Added new Movie Controller, Model and Random view for practice.

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
