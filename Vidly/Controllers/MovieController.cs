59d73de6ea2c76213f669b2b2adbc4520da10670
khannoor710 <khannoor710@gmail.com>
2021-08-08 11:53:37 +0530

    Solution Section 3 - Exercise 3 - Added Movie to data migration. Added new fields in movie model. Created new Genre. Displayed List Of Movies and their details along with Genre

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,21 +12,28 @@ namespace Vidly.Controllers
     public class MovieController : Controller
     {
 
+        private ApplicationDbContext _context;
+        public MovieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ActionResult Index() 
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m=>m.Genre).ToList();
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id) 
         {
-            return new List<Movie>
-            {
-                new Movie{Id = 1, Name = "Shrek"},
-                new Movie{Id = 2, Name = "Wall-e"}
-            };
-        }
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(x => x.Id == id);
+            return View(movie);
+        }        
 
 
         // GET: Movie
