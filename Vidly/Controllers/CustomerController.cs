4b87b63f5ab5cd2b702b0504a1594ce0770f418e
khannoor710 <khannoor710@gmail.com>
2021-08-08 11:27:15 +0530

    Solution Section 3 Exercise 2- Display customers Date of Birth in details page

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -31,7 +31,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id) 
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c =>c.MembershipType).SingleOrDefault(c => c.Id == id);
             return View(customer);
         }
     }
