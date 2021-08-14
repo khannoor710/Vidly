38f10e6794f91d260c6ae32240404c4ad5c6e368
khannoor710 <khannoor710@gmail.com>
2021-08-14 16:29:18 +0530

    Section 4 Exercise - Add a form to create/edit a movie

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -23,6 +23,53 @@ namespace Vidly.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var genre = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genre
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id) 
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Save(Movie movie) 
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.Name = movie.Name;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movie");
+        }
+
         public ActionResult Index() 
         {
             var movies = _context.Movies.Include(m=>m.Genre).ToList();
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -13,14 +13,19 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
-        
-        [Required]
+                
         public Genre Genre { get; set; }
-        
+
+        [Display(Name="Genre")]
+        [Required]
         public byte GenreId { get; set; }
 
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number In Stock")]
         public byte NumberInStock { get; set; }
     }
 }
